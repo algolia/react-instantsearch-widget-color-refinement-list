fc09bf7440fc5ee57408dcfef93b7f7e9e3adc3c
Fabien Motte <fabien.motte@algolia.com>
2021-07-06 14:32:41 +0200

    feat: split styles so they can be imported

--- src/index.tsx
+++ src/index.tsx
@@ -1,3 +1,5 @@
+import './lib/style.css';
+
 export { ColorRefinementListComponent } from './lib/component';
 export { ColorRefinementList } from './lib/widget';
 export { Layout } from './lib/types';
